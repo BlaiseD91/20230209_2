2d68e2a52382dafb9a0efc6be02d7c8a88a7a1de
Kovács Dorina <kovacsdorina2@gmail.com>
2023-02-09 20:25:00 +0100

    Kodolo2

--- Titkosito/src/titkosito/Kodolo.java
+++ Titkosito/src/titkosito/Kodolo.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package titkosito;
 
-/**
- *
- * @author diak
- */
+
 public class Kodolo {
+    
+    private String szo;
+    private int szam=5;
+    
+    public String Kodolo(String szo){
+        
+        char[] tomb = szo.toCharArray();
+        for (int i = 0; i < tomb.length; i++) {
+            tomb[i]= (char)(tomb[i]+5);            
+        }
+        return new String (tomb);
+    }
     
 }
